d41c9aaa3c0c109c4046edcd380747a42404951c
Asish <gitasish1212@gmail.com>
2025-02-14 21:51:31 +0530

    in main.jsx we simple one function and understand what is react element how it is work

--- vite-react/src/main.jsx
+++ vite-react/src/main.jsx
@@ -1,9 +1,37 @@
 import { StrictMode } from 'react'
+import React from 'react';
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function Myapp(){
+    return (
+        <div>
+            <p>custom app || Asish</p>
+        </div>
+    )
+}
+
+// const reactElement = {
+//     type:"a",
+//     props:{
+//         href:'https://google.com',
+//         target:"_blank",
+//     },
+//     children:'click to visit Google'
+// }
+
+const anotherElement=
+(
+    <a href="https://google.com" target="_blank">Visited google</a>
+)
+
+const reactElement =React.createElement('a',{href:'https://google.com',target:"_blank"},'click to visit google');
 createRoot(document.getElementById('root')).render(
   
-    <App />
+    // <>
+    // <App />
+    // <Myapp/>
+    // </>
+    reactElement
   
 )
